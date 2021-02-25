f844194ee7dbcfc88d8b38334d70c5b0890bb577
dapak2002 <dapak@chapman.edu>
2021-02-25 15:33:45 -0800

    Initial commit

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    //Load the file of student names
+    //On button click in UI, randomly pick a student name
+    //Assign to text object in GUI the chosen student name
+
+    public string fileName = "default.txt";
+    public Text displayName;
+
+    private string[] studentNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStudentNames();
+    }
+
+    private void LoadStudentNames()
+    {
+        this.studentNames = File.ReadAllLines(fileName);
+    }
+
+    public void ButtonClick()
+    {
+        int i = UnityEngine.Random.Range(0, this.studentNames.Length); // 25 = 0-24
+        string name = this.studentNames[i];
+        displayName.text = name;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# PakD_Project02
+ Asisgnment 2, Random Name Picker
